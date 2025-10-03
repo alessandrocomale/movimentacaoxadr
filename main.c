02ab50c6d9e6d92e99954b9fdb9a95fbc7b38139
alessandrocomale <202507078518@alunos.estacio.br>
2025-10-03 00:31:15 +0000

    codigo atualizado

--- main.c
+++ main.c
@@ -1,34 +1,81 @@
 #include <stdio.h>
 
+
+
+
+void moverTorre(int movimentos) {
+    if (movimentos == 0) {
+        return; // caso base
+    }
+    printf("Direita\n");
+    moverTorre(movimentos - 1); 
+}
+
+
+void moverBispo(int movimentos) {
+    if (movimentos == 0) {
+        return; // caso base
+    }
+
+    
+    for (int i = 1; i <= 1; i++) { 
+        for (int j = 1; j <= 1; j++) { // movimento horizontal (1 casa por vez)
+            printf("Cima, Direita\n");
+        }
+    }
+
+    moverBispo(movimentos - 1); // chamada recursiva
+}
+
+void moverRainha(int movimentos) {
+    if (movimentos == 0) {
+        return; 
+    }
+    printf("Esquerda\n");
+    moverRainha(movimentos - 1); 
+}
+
+
+
+void moverCavalo() {
+    int movimentosCima = 2;
+    int movimentosDireita = 1;
+
+    printf("\nMovimento do Cavalo:\n");
+
+    for (int i = 1; i <= movimentosCima; i++) {
+        if (i == 1) {
+            printf("Cima\n");
+            continue; 
+        }
+        if (i == 2) {
+            printf("Cima\n");
+        }
+
+        
+        for (int j = 1; j <= movimentosDireita; j++) {
+            printf("Direita\n");
+            break; 
+        }
+    }
+}
+
+
 int main() {
-   
-    int i;
-    int movimentosTorre = 5; 
-    
+    // Torre
     printf("Movimento da Torre:\n");
-    for (i = 1; i <= movimentosTorre; i++) {
-        printf("Direita\n");
-    }
+    moverTorre(5);
 
-   
-    int j = 1;
-    int movimentosBispo = 5; 
-    
+    // Bispo
     printf("\nMovimento do Bispo:\n");
-    while (j <= movimentosBispo) {
-        printf("Cima, Direita\n");
-        j++;
-    }
+    moverBispo(5);
 
-   
-    int k = 1;
-    int movimentosRainha = 8; 
-    
+    // Rainha
     printf("\nMovimento da Rainha:\n");
-    do {
-        printf("Esquerda\n");
-        k++;
-    } while (k <= movimentosRainha);
+    moverRainha(8);
+
+    // Cavalo
+    moverCavalo();
 
     return 0;
 }
